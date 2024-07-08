55196e7748c8e332670e0f0bc2d802556ce0ab31
Jimmy Hsu <jimmyhsu82722@gmail.com>
2024-07-08 15:56:02 +0800

    Enable toggle dark theme

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-const actionIcon = document.querySelector('.cell__action__icon');
-const actionMenu = document.querySelector('.action__menu');
-const toggleMenu = ()=> {
-  actionMenu.classList.toggle('hidden')
-}
+const actionIcon = document.querySelector(".cell__action__icon");
+const actionMenu = document.querySelector(".action__menu");
+const toggleMenu = () => {
+  actionMenu.classList.toggle("hidden");
+};
 
-actionIcon.addEventListener('click', toggleMenu)
+actionIcon.addEventListener("click", toggleMenu);
+
+// dark mode
+// target the switch element
+const darkModeToggle = document.querySelector(
+  ".sidebar__icons--darkmode-input"
+);
+const darkModeToggleHandler = (e) => {
+  console.log('start')
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    console.log(documentElement, ':done')
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    console.log('wrong')
+  }
+};
+
+darkModeToggle.addEventListener("change", darkModeToggleHandler);
